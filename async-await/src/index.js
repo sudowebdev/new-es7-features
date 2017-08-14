82a906aeda1bdcf2edeeaba172c4f9c01d7b2bc6
Nikhil Arora <narora200@gmail.com>
2017-08-14 23:19:59 +0530

    async-await: Example 1 (both cases covered)

--- async-await/src/index.js
+++ async-await/src/index.js
@@ -1,11 +1,6 @@
 const fetch = require("node-fetch");
 
-async function fetchAvatarUrl(userId) {
-    const response = await fetch(`https://catappapi.herokuapp.com/users/${userId}`)
-    const data = await response.json()
-    return data.imageUrl
-}
-
+// Example 1 (Without async-await)
 // function fetchAvatarUrl(userId) {
 // 	return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
 // 		.then(res => {
@@ -18,7 +13,18 @@ async function fetchAvatarUrl(userId) {
 // 			return data.imgUrl;
 // 		});
 // }
+// const result = fetchAvatarUrl(123)
+
+
+// Example 1 (With async-await)
+// async function fetchAvatarUrl(userId) {
+//     const response = await fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+//     const data = await response.json()
+//     return data.imageUrl
+// }
+// const result = fetchAvatarUrl(123);
+// result.then(imgUrl => {console.log(imgUrl)})
+
+
 
   
-const result = fetchAvatarUrl(123);
-result.then(imgUrl => {console.log(imgUrl)})
